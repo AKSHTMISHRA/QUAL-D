572f08ccfb4515b256e14ded0933b546b91ca3c6
Akshat Mishra <akshatnaveenmishra@gmail.com>
2024-09-28 22:16:40 +0530

    Version 1.0.0

--- src/MainPage.py
+++ src/MainPage.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
+from streamlit_extras.stylable_container import stylable_container
 from src.pages.Home import Home
 from src.pages.Upload import Upload
 from src.pages.ContactUs import ContactMe
@@ -11,35 +12,61 @@ class MainPage:
 
     def MainPage():
         st.set_page_config(
-            page_title='dataquality',
+            page_title='Qual-D',
             layout='wide',            
         )
-        st.markdown(""" 
-            <h1 style="background-color: #112639; border: 1px solid black; color: white; font-family: 'Times New Roman', serif; text-align: center; padding: 9px;">
-                Qual-D
-            </h1>
-            """,unsafe_allow_html=True)
-
-        
-        selected = option_menu(None, ["What's New", "Upload","Filters", "Contact Us"],  
+        with stylable_container(
+        key="header_classic",
+        css_styles="""{
+        background-color: #112639;
+        color: white;
+        font-family:roboto;
+        padding: 10px;
+        border-bottom: 2px solid #007bff;
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+        position: fixed;
+        top : 60px;
+        left:0;
+        width: 100%;    
+        z-index: 10000;
+        }""",):
+            col1,col2=st.columns([2,3])
+            with col1:
+                st.write("""<b style="color: white; font-size: 42px">Qual-D</b>""",unsafe_allow_html=True)
+            
+            with col2:
+                selected = option_menu(None, ["What's New", "Upload","Filters", "Contact Us"],  
                 icons=['house', 'speedometer','table' ,'chat-left'], 
-                menu_icon="cast", default_index=0, orientation="horizontal",
+               menu_icon="cast", default_index=0, orientation="horizontal",
                 styles={
-                    "container": {"padding": "0!important", "background-color": "#d7d9de"},
-                    "icon": {"color": "white", "font-size": "25px", "display": "block", "text-align": "center"},
-                    "nav-link": {"font-size": "16px", "text-align": "center", "margin": "0px", "color": "black", "--hover-color": "rgba(255, 87, 51,0.5)","padding":"3px"},
-                    "nav-link-selected": {"background-color": "#FF5733", "color": "white", "display": "block"},
+                    "container": {"padding": "0!important", "background-color": "#112639","border-radius": "0px","box-shadow": "none" ,"margin": "0px","border": "none","overflow": "hidden","padding-top":"50px"},
+                    "icon": {"color": "white", "display": "block", "text-align": "center","border": "none","padding": "0px","font-size":"30px"},
+                    "nav-link": {"font-size": "16px", "text-align": "center", "margin": "0px", "color": "white", "--hover-color": "rgba(255, 87, 51,0.5)","padding": "0px","border": "none"},
+                    "nav-link-selected": {"background-color": "#ff6804", "color": "white", "display": "block"},
                     "nav-link-icon": {"display": "block", "text-align": "center"}
                 }
             )
 
-        if selected=="What's New":
-            Home.Home()
-        if selected=='Upload':
-            Upload.Upload()
-        if selected=='Contact Us':
-            ContactMe.ContactMePage()
-        if selected=='Filters':
-            Filter.FilterPage()    
+        with stylable_container(
+        key="body",
+        css_styles="""{
+        position : relative;
+        top: 60px;
+        left: 0;
+        width: 100%;
+        
+        z-index: 100;
+        }""",):
+            if selected=="What's New":
+                Home.Home()
+            if selected=='Upload':
+                Upload.Upload()
+            if selected=='Contact Us':
+                ContactMe.ContactMePage()
+            if selected=='Filters':
+                Filter.FilterPage() 
+        
+
+           
 
 
--- src/pages/Filter.py
+++ src/pages/Filter.py
@@ -1,20 +1,107 @@
-import streamlit as st
 from src.pages.Upload import Upload
+import streamlit as st
+import pandas as pd
+from typing import Any, Dict
+from pandas.api.types import is_categorical_dtype, is_numeric_dtype, is_datetime64_any_dtype, is_object_dtype
 
 class Filter:
-    def __init__(self):
-        pass
+    @staticmethod
+    def dataframe_explorer(df: pd.DataFrame, case: bool = True) -> pd.DataFrame:
+        """
+        Adds a UI on top of a dataframe to let viewers filter columns.
 
+        Args:
+            df (pd.DataFrame): Original dataframe
+            case (bool, optional): If True, text inputs will be case sensitive. Defaults to True.
+
+        Returns:
+            pd.DataFrame: Filtered dataframe
+        """
+
+        random_key_base = pd.util.hash_pandas_object(df)
+
+        df = df.copy()
+
+        # Try to convert datetimes into standard format (datetime, no timezone)
+        for col in df.columns:
+            if is_object_dtype(df[col]):
+                try:
+                    df[col] = pd.to_datetime(df[col])
+                except Exception:
+                    pass
+
+            if is_datetime64_any_dtype(df[col]):
+                df[col] = df[col].dt.tz_localize(None)
+
+        modification_container = st.container()
+
+        with modification_container:
+            to_filter_columns = st.multiselect(
+                "Filter dataframe on",
+                df.columns,
+                key=f"{random_key_base}_multiselect",
+            )
+            filters: Dict[str, Any] = dict()
+            for column in to_filter_columns:
+                left, right = st.columns((1, 20))
+                # Treat columns with < 40 unique values as categorical
+                if is_categorical_dtype(df[column]) or df[column].nunique() < 40:
+                    filters[column] = right.multiselect(
+                        f"Values for {column}",
+                        df[column].unique(),
+                        default=list(df[column].unique()),
+                        key=f"{random_key_base}_{column}",
+                    )
+                    df = df[df[column].isin(filters[column])]
+                elif is_numeric_dtype(df[column]):
+                    _min = float(df[column].min())
+                    _max = float(df[column].max())
+                    step = (_max - _min) / 100
+                    filters[column] = right.slider(
+                        f"Values for {column}",
+                        _min,
+                        _max,
+                        (_min, _max),
+                        step=step,
+                        key=f"{random_key_base}_{column}",
+                    )
+                    df = df[df[column].between(*filters[column])]
+                elif is_datetime64_any_dtype(df[column]):
+                    filters[column] = right.date_input(
+                        f"Values for {column}",
+                        value=(
+                            df[column].min(),
+                            df[column].max(),
+                        ),
+                        key=f"{random_key_base}_{column}",
+                    )
+                    if len(filters[column]) == 2:
+                        filters[column] = tuple(map(pd.to_datetime, filters[column]))
+                        start_date, end_date = filters[column]
+                        df = df.loc[df[column].between(start_date, end_date)]
+                else:
+                    filters[column] = right.text_input(
+                        f"Pattern in {column}",
+                        key=f"{random_key_base}_{column}",
+                    )
+                    if filters[column]:
+                        df = df[df[column].str.contains(filters[column], case=case)]
+
+        return df
+
+    @staticmethod
     def FilterPage():
-        col1,col2=st.columns([1,3])
+        col1, col2, col3 = st.columns([0.5, 3, 0.5])
 
         with col2:
             if 'UploadedDf' in st.session_state:
-                df=st.session_state.UploadedDf
-                st.subheader('DATA:')
-                st.dataframe(df,hide_index=True)
+                df = st.session_state.UploadedDf
+                st.subheader('Filters:')
+                filtered_df = Filter.dataframe_explorer(df, case=False)  # No 'self' needed
+                st.dataframe(filtered_df, use_container_width=True)
             else: 
                 st.subheader("You have not Uploaded any File yet")
         with col1:
-            None            
-
+            pass
+        with col3:
+            pass
